ad34c82b4547ef69c4e6a63700c73a8b65a4c41d
Vermzzyy <cpt.woof16@gmail.com>
2026-05-09 00:08:34 +0800

    fixed deployment errors

--- src/main/java/com/creatorshub/creatorshub/shared/config/CorsConfig.java
+++ src/main/java/com/creatorshub/creatorshub/shared/config/CorsConfig.java
@@ -6,21 +6,38 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.Arrays;
+
 @Configuration
 public class CorsConfig {
 
     @Bean
     public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
         CorsConfiguration config = new CorsConfiguration();
-        
+
+        config.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                "https://creatorshub-lilac.vercel.app"
+        ));
+
+        config.setAllowedMethods(Arrays.asList(
+                "GET",
+                "POST",
+                "PUT",
+                "DELETE",
+                "OPTIONS"
+        ));
+
+        config.setAllowedHeaders(Arrays.asList("*"));
+
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:3000");
-        config.addAllowedOrigin("https://creatorshub-lilac.vercel.app");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
         source.registerCorsConfiguration("/**", config);
+
         return new CorsFilter(source);
     }
 }
