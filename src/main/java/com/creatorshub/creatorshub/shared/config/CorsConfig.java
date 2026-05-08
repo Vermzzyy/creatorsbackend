0f4c38e123c3826360690389331b64cfa4459862
Vermzzyy <cpt.woof16@gmail.com>
2026-05-08 23:25:33 +0800

    fixed for deployment

--- src/main/java/com/creatorshub/creatorshub/shared/config/CorsConfig.java
+++ src/main/java/com/creatorshub/creatorshub/shared/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000", "https://creatorshub-lilac.vercel.app")
                         .allowedMethods("*")
                         .allowedHeaders("*");
 
